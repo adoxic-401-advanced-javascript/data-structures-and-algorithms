30c85582b4537942b29847c49cdff9c6bd69c2a7
adoxic <griffinmaeve@gmail.com>
2019-10-05 15:10:37 -0700

    think it's working

---
+++ Chalenges/_test_/queue-with-stacks.test.js
@@ -0,0 +1,25 @@
+const PseudoQueue = require('../queueWithStacks/queue-with-stacks')
+
+describe('fake queue', () => {
+  it('add stuff queue style', () => {
+    const fakeQ = new PseudoQueue();
+    fakeQ.enqueue('first');
+    fakeQ.enqueue('second');
+
+    expect(fakeQ.inStack.stack.next.value).toBe('first');
+    expect(fakeQ.inStack.stack.value).toBe('second');
+  });
+
+  it('remove stuff queue style', () => {
+    const fakeQ = new PseudoQueue();
+    fakeQ.enqueue('first');
+    fakeQ.enqueue('second');
+    const first = fakeQ.dequeue();
+    const second = fakeQ.dequeue();
+    const empty = fakeQ.dequeue();
+
+    expect(first).toBe('first');
+    expect(second).toBe('second');
+    expect(empty).toBe('this is an empty stack');
+  });
+});
