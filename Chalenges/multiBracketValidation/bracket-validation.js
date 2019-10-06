c9d350764ece91fcd10cf64f16bf5bcb9809ab55
adoxic <griffinmaeve@gmail.com>
2019-10-05 22:29:05 -0700

    tests work

---
+++ Chalenges/multiBracketValidation/bracket-validation.js
@@ -0,0 +1,45 @@
+function bracketValidation(str) {
+  const brackets = {
+    '{': 0,
+    '}': 0,
+    '[': 0,
+    ']': 0,
+    '(': 0,
+    ')': 0,
+  };
+  let good = 0;
+  let bad = 0;
+
+  for(let i = 0; i < str.length; i++) {
+    const letter = str[i];
+    if(!brackets.letter) {
+      brackets[letter] = 1;
+    } else {
+      brackets.letter++;
+    }
+  }
+
+  const keys = Object.keys(brackets);
+  const values = Object.values(brackets);
+
+  for(let i = 0; i < keys.length; i += 2) {
+    if(values[i] === values[i + 1] && values[i] > 0) {
+      good++;
+    } else if(values[i] + values[i + 1] > 0){
+      bad++;
+    }    
+  }
+  console.log(good);
+  console.log(bad);
+
+  if(bad > 0) {
+    return false;
+  } else if(good > 0) {
+    return true;
+  } else {
+    return false;
+  }
+  
+}
+
+module.exports = bracketValidation;
