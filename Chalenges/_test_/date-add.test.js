9d2d671232b71fe8048c9c474c732814aa75a793
adoxic <griffinmaeve@gmail.com>
2019-10-21 22:14:17 -0700

    it's fine

--- Chalenges/_test_/date-add.test.js
+++ Chalenges/_test_/date-add.test.js
@@ -16,4 +16,11 @@ describe('can add to dates by unit?', () => {
     const expected = new Date('2019-10-29T03:10:50.616Z');
     expect(result).toEqual(expected);
   });
+  it('should add year, 3 weeks, 15 minutes', () => {
+    const today = Date.parse('2019-10-22T00:10:50.616Z');
+   
+    const result = dateAdd(today, '1.y 3.w 15.m');
+    const expected = new Date('2020-10-13T00:25:50.616Z');
+    expect(result).toEqual(expected);
+  });
 });
