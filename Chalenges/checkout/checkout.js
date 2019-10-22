587f8e40fd469d4605055f3ba0c6cd9017f69c50
adoxic <griffinmaeve@gmail.com>
2019-10-22 15:30:03 -0700

    ugh fine I put it in the right order

--- Chalenges/checkout/checkout.js
+++ Chalenges/checkout/checkout.js
@@ -4,8 +4,13 @@ function sortByDay(arr) {
     const object = arr[i];
     const newDay = new Date(object.timestamp);
 
-    const timeString = newDay.toUTCString().replace(',', '').substr(0, 15);
-    
+    const UTC = newDay.toUTCString().replace(',', '').substr(0, 15);
+    const timeArr = UTC.split(' ');
+    const day = timeArr[1];
+    const month = timeArr[2];
+    timeArr[2] = day;
+    timeArr[1] = month;
+    const timeString = timeArr.join(' ');
     
     if(!storage[timeString])  {
       storage[timeString] = object.price;
