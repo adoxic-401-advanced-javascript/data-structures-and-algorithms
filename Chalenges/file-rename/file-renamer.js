8aad1719b89b812bfe726da57062f445c63cb7f7
adoxic <griffinmaeve@gmail.com>
2019-10-24 13:17:41 -0700

    solution and steps

---
+++ Chalenges/file-rename/file-renamer.js
@@ -0,0 +1,20 @@
+//Solution, use node fs to read and rename files.
+//1 put all files in a folder.
+
+//2 require fs with async.
+//2.5 setup fs mock in jest
+
+//3 use readdir to read and save file names into an fileName array.
+//test
+
+//4 loop through fileName to make the path to readFile.
+//test
+
+//5 get the current time with new Date()
+//test
+
+//6 take Date, current index of fileName, and contents of the of readFile put into a string to creat newName string
+//test
+
+//7 use newName and rename to rename the file.
+//test
