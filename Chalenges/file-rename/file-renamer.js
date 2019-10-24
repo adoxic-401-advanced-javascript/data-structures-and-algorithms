a18fee0b001548172c71a6a77cecdd2e58203c5a
adoxic <griffinmaeve@gmail.com>
2019-10-24 15:03:41 -0700

    first draft of readdir function

--- Chalenges/file-rename/file-renamer.js
+++ Chalenges/file-rename/file-renamer.js
@@ -7,6 +7,18 @@ const fs = require('fs').promises;
 
 //3 use readdir to read and save file names into an fileName array.
 //test
+function fileNameGetter(path) {
+  const fileNames = [];
+  return fs.readdir(path, 'utf8')
+    .then(array => {
+      console.log(array);
+      array.map(name => {
+        fileNames.push(name);
+      });
+      return fileNames;
+    });
+  
+}
 
 //4 loop through fileName to make the path to readFile.
 //test
@@ -19,3 +31,5 @@ const fs = require('fs').promises;
 
 //7 use newName and rename to rename the file.
 //test
+
+module.exports = { fileNameGetter };
