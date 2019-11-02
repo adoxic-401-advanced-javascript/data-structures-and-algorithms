1c2839afdc57e0db76dcd99c2d62429b6e2a5fb4
adoxic <griffinmaeve@gmail.com>
2019-11-02 13:06:48 -0700

    rename for clarity

--- Chalenges/_test_/merge-sort.test.js
+++ Chalenges/_test_/merge-sort.test.js
@@ -3,7 +3,7 @@ const mergeSort = require('../mergeSort/merge-sort');
 describe('Merge sort arr', () => {
   it('should sort array', () => {
     const arr = [7, 80, 21, 35, 1, 28, 4, 44];
-    const result = mergeSort(arr);
-    expect(result).toEqual([1, 4, 7, 21, 28, 35, 44, 80]);
+    mergeSort(arr);
+    expect(arr).toEqual([1, 4, 7, 21, 28, 35, 44, 80]);
   });
 });
--- Chalenges/mergeSort/merge-sort.js
+++ Chalenges/mergeSort/merge-sort.js
@@ -33,44 +33,47 @@
 //        set remaining entries in arr to remaining values in left
 
 const mergeSort = (arr) => {
-  console.log('start arr', arr);
+  
   const n = arr.length;
   
   if(n > 1) {
+    const save = arr.slice();
     const mid = Math.floor(n / 2);
-    const left = arr.splice(0, mid);
-    const right = arr;
-    console.log(left, right, mid);
+    const left = save.splice(0, mid);
+    const right = save;
+  
     mergeSort(left);
     mergeSort(right);
 
     merge(left, right, arr);
   }
 
-  return arr;
+  
 };
 
 const merge = (left, right, arr) => {
-  let i = 0;
-  let j = 0;
-  let k = 0;
+  let iArr = 0;
+  let iLeft = 0;
+  let iRight = 0;
 
-  while(i < left.length && j < right.length) {
-    if(left[i] <= right[j]) {
-      arr[k] = left[i];
-      i = i + 1;
+  while(iLeft < left.length && iRight < right.length) {
+    if(left[iLeft] <= right[iRight]) {
+      arr[iArr] = left[iArr];
+      iRight = iRight + 1;
     } else {
-      arr[k] = left[i];
-      j = j + 1;
+      arr[iArr] = left[iLeft];
+      iLeft = iLeft + 1;
     } 
-    k = k + 1;
+    iArr = iArr + 1;
   }
 
-  if(i === left.length) {
-    left = right;
+  if(iArr === left.length) {
+    right.concat(arr);
   } else {
-    right = left;
+    left.concat(arr);
   }
+  console.log('is arr getting stuff', arr);
+  return arr;
 };
 
 module.exports = mergeSort;
