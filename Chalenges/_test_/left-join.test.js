6fac4a5613d12070c898afdb03502856b94a4d82
adoxic <griffinmaeve@gmail.com>
2019-11-06 11:15:37 -0800

    works in first test but also found a problem

--- Chalenges/_test_/left-join.test.js
+++ Chalenges/_test_/left-join.test.js
@@ -4,5 +4,16 @@ const leftJoin = require('../leftJoin/left-join');
 describe('Left Join', () => {
   const hashOne = new HashTable();
   const hashTwo = new HashTable();
-  
+  hashOne.add('happy', 'delighted');
+  hashTwo.add('happy', 'sad');
+  hashOne.add('sad', 'grim');
+  hashTwo.add('sad', 'happy');
+  hashOne.add('angry', 'furious');
+  hashTwo.add('angry', 'forgiving');
+
+  it('should join without errors', () => {
+    leftJoin(hashOne, hashTwo);
+    const happyVal = hashOne.get('happy');
+    expect(happyVal).toBe('delighted sad');
+  });
 });
--- Chalenges/leftJoin/left-join.js
+++ Chalenges/leftJoin/left-join.js
@@ -1,12 +1,15 @@
 const leftJoin = (hashMap1, hashMap2) => {
   for(let i = 0; i < hashMap2.buckets.length; i++) {
     const bucket = hashMap2.buckets[i];
-    const hash = hashMap2.hash(bucket[0]);
-    if(hashMap1.contains(hash)) {
-      const firstContent = hashMap1.get(bucket[0]);
-      const secondContent = bucket[1];
-      firstContent.concat(' ', secondContent);
-      hashMap1.set(bucket[0], firstContent);
+    if(bucket !== undefined) {
+      
+      if(hashMap1.contains(bucket[0])) {
+        console.log('is getting into if');
+        const firstContent = hashMap1.get(bucket[0]);
+        const secondContent = bucket[1];
+        const newContent = firstContent.concat(' ', secondContent);
+        hashMap1.add(bucket[0], newContent);
+      }
     }
   }
   return hashMap1;
