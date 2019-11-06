9923b2431695632eb83f2fd7ee932c3c88637147
adoxic <griffinmaeve@gmail.com>
2019-11-06 11:24:26 -0800

    fixed

--- Chalenges/_test_/left-join.test.js
+++ Chalenges/_test_/left-join.test.js
@@ -11,9 +11,29 @@ describe('Left Join', () => {
   hashOne.add('angry', 'furious');
   hashTwo.add('angry', 'forgiving');
 
+  it('should check state before left join', () => {
+    const happyVal = hashOne.get('happy');
+    const sadVal = hashOne.get('sad');
+    const angryVal = hashOne.get('angry');
+    expect(happyVal).toBe('delighted');
+    expect(sadVal).toBe('grim');
+    expect(angryVal).toBe('furious');
+  });
+
   it('should join without errors', () => {
     leftJoin(hashOne, hashTwo);
     const happyVal = hashOne.get('happy');
+    const sadVal = hashOne.get('sad');
+    const angryVal = hashOne.get('angry');
     expect(happyVal).toBe('delighted sad');
+    expect(sadVal).toBe('grim happy');
+    expect(angryVal).toBe('furious forgiving');
+  });
+
+  it('should add null if there is not value in the second Hash Table', () => {
+    hashOne.add('peppy', 'energetic');
+    leftJoin(hashOne, hashTwo);
+    const peppyVal = hashOne.get('peppy');
+    expect(peppyVal).toBe('energetic null');
   });
 });
--- Chalenges/leftJoin/left-join.js
+++ Chalenges/leftJoin/left-join.js
@@ -1,12 +1,17 @@
 const leftJoin = (hashMap1, hashMap2) => {
-  for(let i = 0; i < hashMap2.buckets.length; i++) {
-    const bucket = hashMap2.buckets[i];
+  for(let i = 0; i < hashMap1.buckets.length; i++) {
+    const bucket = hashMap1.buckets[i];
     if(bucket !== undefined) {
       
-      if(hashMap1.contains(bucket[0])) {
-        console.log('is getting into if');
-        const firstContent = hashMap1.get(bucket[0]);
-        const secondContent = bucket[1];
+      if(hashMap2.contains(bucket[0])) {
+        
+        const firstContent = bucket[1];
+        const secondContent = hashMap2.get(bucket[0]);
+        const newContent = firstContent.concat(' ', secondContent);
+        hashMap1.add(bucket[0], newContent);
+      } else {
+        const firstContent = bucket[1];
+        const secondContent = 'null';
         const newContent = firstContent.concat(' ', secondContent);
         hashMap1.add(bucket[0], newContent);
       }
